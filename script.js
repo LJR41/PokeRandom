1f05aab3d36ecffaaa9c88ca1344c271827264c1
LJR411~ <ljremulla@yahoo.com>
2023-06-30 09:26:40 -0700

    comment changes

--- script.js
+++ script.js
@@ -535,7 +535,7 @@ function getFinal() {
         modal.style.display = "block";
     }
 }
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x), close it
 span.onclick = function() {
 modal.style.display = "none"
 }
